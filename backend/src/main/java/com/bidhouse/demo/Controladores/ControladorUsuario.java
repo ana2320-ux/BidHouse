295509d55a81d871e4e6f28d54fa9134fa0aead2
AnaMurcia <anamur2320@gmail.com>
2026-09-19 16:39:34 -0500

    Backend: Controlador Usuario

---
+++ backend/src/main/java/com/bidhouse/demo/Controladores/ControladorUsuario.java
@@ -0,0 +1,26 @@
+package com.bidhouse.demo.Controladores;
+
+import com.bidhouse.demo.Modelos.Usuario;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:5173") // Permite que tu frontend de Vite se conecte
+public class ControladorUsuario {
+
+    @GetMapping("/perfil")
+    public Usuario obtenerPerfilActual() {
+        // Simulamos el usuario que tienes en tu diseño actual
+        return new Usuario(
+                "Alejandro",
+                "Montes",
+                "alejandro@bidhouse.com",
+                "secreto123",
+                "alemontes",
+                "Cuenta de Custodia de Activos Reales en Operación • Premium Trader"
+        );
+    }
+}
